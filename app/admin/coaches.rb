2c13070e001a5918cbc6d26462a345a4b49ffac7
Raghu Betina <raghu@firstdraft.com>
2018-03-02 15:25:28 +0000

    Whitelist columns in Coach for ActiveAdmin

--- app/admin/coaches.rb
+++ app/admin/coaches.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Coach do
+
+ permit_params :user_id, :improvement_plan_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
