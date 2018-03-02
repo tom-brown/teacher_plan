00ca0e9a0bef429a3645f370f0e78e476d1f93b4
Raghu Betina <raghu@firstdraft.com>
2018-03-02 15:24:54 +0000

    Direct association between improvement_plans and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment < ApplicationRecord
   # Direct associations
 
+  belongs_to :improvement_plan,
+             :required => false
+
   # Indirect associations
 
   # Validations
--- app/models/improvement_plan.rb
+++ app/models/improvement_plan.rb
@@ -1,6 +1,9 @@
 class ImprovementPlan < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :destroy
+
   belongs_to :user
 
   # Indirect associations
